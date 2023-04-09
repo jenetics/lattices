33e4fba2e337fa6e6e45795101406ec4a1ca6f59
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2023-04-09 09:05:09 +0200

    Add 'LongArray'.

--- lattices/src/main/java/io/jenetics/lattices/array/DenseLongArray.java
+++ lattices/src/main/java/io/jenetics/lattices/array/DenseLongArray.java
@@ -1,22 +1,3 @@
-/*
- * Java Lattice Library (@__identifier__@).
- * Copyright (c) @__year__@ Franz Wilhelmstötter
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Author:
- *    Franz Wilhelmstötter (franz.wilhelmstoetter@gmail.com)
- */
 package io.jenetics.lattices.array;
 
 import static java.util.Objects.checkFromIndexSize;
@@ -25,6 +6,7 @@ import static java.util.Objects.requireNonNull;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 /**
  * Implementation of a <em>dense</em> array of {@code int} values.
@@ -37,20 +19,20 @@ import java.util.stream.IntStream;
  * @since 3.0
  * @version 3.0
  */
-public record DenseIntArray(int[] elements, int from, int length)
-    implements IntArray
+public record DenseLongArray(long[] elements, int from, int length)
+    implements LongArray
 {
 
     /**
-     * Create a new <em>dense</em> int array with the given values
+     * Create a new <em>dense</em> long array with the given values
      *
-     * @param elements the underlying {@code int} element values
+     * @param elements the underlying {@code long} element values
      * @param from the index of the first array element (inclusively)
      * @param length the length of the sub-array
      * @throws IndexOutOfBoundsException if the given {@code from} value and
      *         {@code length} is out of bounds
      */
-    public DenseIntArray {
+    public DenseLongArray {
         requireNonNull(elements);
         checkFromIndexSize(from, length, elements.length);
     }
@@ -58,31 +40,31 @@ public record DenseIntArray(int[] elements, int from, int length)
     /**
      * Create a new <em>dense</em> int array with the given values
      *
-     * @param elements the underlying {@code int} element values
+     * @param elements the underlying {@code long} element values
      * @param from the index of the first array element (inclusively)
      * @throws IndexOutOfBoundsException if the given {@code from} value is out
      *         of bounds
      */
-    public DenseIntArray(final int[] elements, int from) {
+    public DenseLongArray(final long[] elements, int from) {
         this(elements, from, elements.length - from);
     }
 
     /**
-     * Create a new <em>dense</em> array of {@code int} values.
+     * Create a new <em>dense</em> array of {@code long} values.
      *
-     * @param elements the underlying {@code int} element values
+     * @param elements the underlying {@code long} element values
      */
-    public DenseIntArray(final int[] elements) {
+    public DenseLongArray(final long[] elements) {
         this(elements, 0, elements.length);
     }
 
     @Override
-    public int get(final int index) {
+    public long get(final int index) {
         return elements[index + from];
     }
 
     @Override
-    public void set(final int index, final int value) {
+    public void set(final int index, final long value) {
         elements[index + from] = value;
     }
 
@@ -92,50 +74,50 @@ public record DenseIntArray(int[] elements, int from, int length)
     }
 
     @Override
-    public DenseIntArray copy() {
+    public DenseLongArray copy() {
         final var elems = Arrays.copyOfRange(elements, from, from + length);
-        return new DenseIntArray(elems);
+        return new DenseLongArray(elems);
     }
 
     @Override
-    public DenseIntArray copy(final int start, final int length) {
+    public DenseLongArray copy(final int start, final int length) {
         final var array = Arrays.copyOfRange(
             elements,
             start + from, start + from + length
         );
-        return new DenseIntArray(array);
+        return new DenseLongArray(array);
     }
 
     @Override
-    public DenseIntArray like(final int length) {
+    public DenseLongArray like(final int length) {
         return ofSize(length);
     }
 
     /**
-     * Return an int stream from the given array.
+     * Return {@code long}  stream from the given array.
      *
-     * @return an int stream from the given array
+     * @return an {@code long} stream from the given array
      */
-    public IntStream stream() {
+    public LongStream stream() {
         return IntStream.range(0, length())
-            .map(this::get);
+            .mapToLong(this::get);
     }
 
     @Override
     public String toString() {
         return stream()
-            .mapToObj(Integer::toString)
+            .mapToObj(Long::toString)
             .collect(Collectors.joining(", ", "[", "]"));
     }
 
     /**
-     * Create a new dense {@code int} array with the given {@code length}.
+     * Create a new dense {@code long} array with the given {@code length}.
      *
      * @param length the length of the created array
-     * @return a new dense {@code int} array with the given {@code length}
+     * @return a new dense {@code long} array with the given {@code length}
      */
-    public static DenseIntArray ofSize(final int length) {
-        return new DenseIntArray(new int[length]);
+    public static DenseLongArray ofSize(final int length) {
+        return new DenseLongArray(new long[length]);
     }
 
 }
